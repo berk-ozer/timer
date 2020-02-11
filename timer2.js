a51bad8c889ad84236d622ad7073e222ff09077a
Galip Berk Ozer <galipberkozer@gmail.com>
2020-02-11 19:02:16 +0000

    Exit message when pressed ctrl c

--- timer2.js
+++ timer2.js
@@ -6,6 +6,7 @@ stdin.on('data', (key) => {
   if (key === 'b') {
     process.stdout.write('\x07');
   } else if (key === '\u0003') {
+    process.stdout.write('Thanks for using me, ciao!\n');
     process.exit();
   }
 });
